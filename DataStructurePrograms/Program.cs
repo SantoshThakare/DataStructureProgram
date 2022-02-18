8390ae0f3a2426ed50c6400da02b220a70fe7a6a
SantoshThakare <santoshthakare50@gmail.com>
2022-02-19 00:00:17 +0530

    Balance Paranthesis program

---
+++ DataStructurePrograms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DataStructure Programs");
+            Console.WriteLine("Enter option 1");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    char[] expression = "(5+6)∗(7+8)/(4+3)(5+6)∗(7+8)/(4+3)".ToCharArray();
+                    BalancingParanthesis.StackOperations stackOperations = new BalancingParanthesis.StackOperations();
+                    bool result = stackOperations.Isbalanced(expression);
+                    if (result)
+                    {
+                        Console.WriteLine("Parantheses Balanced");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parantheses not Balanced");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
